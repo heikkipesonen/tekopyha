200c35b22385508f67e125d47d6b7a0b8b8c768f
Heikki Pesonen <heikki.pesonen@luotta.fi>
2015-11-15 18:32:14 +0200

    hax

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ class Header extends React.Component {
   render () {
     return (
       <div className="header">
-        <ImageContainer className="header-image" src={this.props.image}></ImageContainer>
+        
         <div className="header-text-container">
           <h2 className="header-title">{this.props.title}</h2>
           <p className="header-text">{this.props.text}</p>
--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -6,10 +6,22 @@ class Item extends React.Component {
     super(props);
   }
 
+  support = () => {
+    let item = this.props.src;
+  }
+
+  componentWillReceiveProps () {
+    console.log('PERDE');
+  }
+
   render () {
-    console.log(this.props.src);
+    let classNames = this.props.className + ' item';
+    if (this.props.src && (this.props.src.votes > this.props.src.votes_required)){
+      classNames += ' item-complete';
+    }
+
     return (
-      <div className="item">
+      <div className={classNames}>
         <div className="item-title-wrapper">
           <h3 className="item-title">{this.props.src.title}</h3>
           <p className="item-description">{this.props.src.description}</p>
@@ -19,10 +31,11 @@ class Item extends React.Component {
             <Barchart value={this.props.src.votes} total={this.props.src.votes_required} label={this.props.src.votes}></Barchart>
           </div>
           <div className="item-actions">
-            <button className="button button-action">{this.props.src.action}</button>
+            <button className="button button-action" onClick={this.support}><i className="icon ion-ios-heart"></i>{this.props.src.action}</button>
           </div>
         </div>
         <div className="item-share">
+          <p className="share-text">Kerro urotöistäsi myös muille, ettei panoksesi valu hukkaan!</p>
           <i className="icon ion-social-facebook item-button-share"></i>
           <i className="icon ion-social-twitter item-button-share"></i>
         </div>
